51c8a8dc51537177c0b0da0cc15aeba202aab3dc
Imran Chowdhury <144194950+imranncc@users.noreply.github.com>
2024-02-15 13:54:21 +0530

    Add files via upload

---
+++ src/SumTwoNumbers.java
@@ -0,0 +1,23 @@
+/* Assignment 1 (100 marks in total; 5% of the final score of this course) 
+ * 
+ * Question 2 (20 marks)
+ * 
+ * Write a Java program to print the sum of two user input integer numbers.
+ * Also submit a screenshot to demonstrate that you have successfully ran
+ * javac and java commandline to compile and run the code of Q1_HelloWorld.java
+ * 
+ * INPUT: two user input integer numbers
+ * OUTPUT: the sum of the two user input integer numbers
+ * 
+ * Hint1: you may use java.util.Scanner to take user input numbers.
+ * 
+ */
+
+import java.util.Scanner;
+
+
+public class Q2_SumTwoNumbers {
+	public static void main(String[] args) {
+		/* Place your code here */
+	}
+}
